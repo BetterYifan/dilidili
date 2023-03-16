a88df80bc49b137e919ee3b35f64ed573d931180
BetterYifan <1197688675@qq.com>
2023-03-16 22:49:54 +0800

    restTemplate初始化

---
+++ filter-service/src/main/java/com/dilidili/filter/service/config/WebMvcConfiguration.java
@@ -0,0 +1,9 @@
+package com.dilidili.filter.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+}
--- filter-service/src/main/java/com/dilidili/filter/service/interceptor/InBodyFilter.java
+++ filter-service/src/main/java/com/dilidili/filter/service/interceptor/InBodyFilter.java
@@ -7,9 +7,25 @@ import javax.servlet.*;
 import java.io.IOException;
 
 @Component
-public class InBodyFilter implements Filter{
+@WebFilter(filterName = "inBody")
+public class InBodyFilter implements Filter {
+
+    /**
+     * 只在项目启动的时候执行一次
+     *
+     * @param filterConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("inbody filter init");
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        System.out.println("begin filter");
+        filterChain.doFilter(servletRequest, servletResponse);
+        // 执行完业务handler后执行的方法
+        System.out.println("end filter");
     }
 }
